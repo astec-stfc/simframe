Fix units errors

SimulationFramework/Modules/Beams/Particles/__init__.py
@@ -7,7 +7,7 @@ from .twiss import twiss as twissobject
 from .slice import slice as sliceobject
 from .sigmas import sigmas as sigmasobject
 from .centroids import centroids as centroidsobject
-from ...units import UnitValue, mean, cov
+from ...units import UnitValue
 
 class Particles(Munch):
 
@@ -77,7 +77,7 @@ class Particles(Munch):
             warnings.simplefilter("ignore")
             ans = 0
             if len(u) > 1 and len(up) > 1:
-                ans = cov([u,up])[0,1]
+                ans = UnitValue(np.cov([u,up])[0,1], unit_multiply(u.units, up.units, divide=False))
             return ans
 
     def eta_correlation(self, u):

SimulationFramework/Modules/Beams/Particles/centroids.py
@@ -1,7 +1,7 @@
 import munch
 import numpy as np
 from ... import constants
-from ...units import UnitValue, mean, covariance
+from ...units import UnitValue
 
 class centroids(munch.Munch):
 
@@ -35,25 +35,25 @@ class centroids(munch.Munch):
 
     @property
     def Cx(self):
-        return mean(self.beam.x)
+        return np.mean(self.beam.x)
     @property
     def Cy(self):
-        return mean(self.beam.y)
+        return np.mean(self.beam.y)
     @property
     def Cz(self):
-        return mean(self.beam.z)
+        return np.mean(self.beam.z)
     @property
     def Ct(self):
-        return mean(self.beam.t)
+        return np.mean(self.beam.t)
     @property
     def Cp(self):
-        return mean(self.beam.cp)
+        return np.mean(self.beam.cp)
     @property
     def Cpx(self):
-        return mean(self.beam.cpx)
+        return np.mean(self.beam.cpx)
     @property
     def Cpy(self):
-        return mean(self.beam.cpy)
+        return np.mean(self.beam.cpy)
     @property
     def Cpz(self):
-        return mean(self.beam.cpz)
+        return np.mean(self.beam.cpz)

SimulationFramework/Modules/Beams/Particles/emittance.py
@@ -1,5 +1,5 @@
 import numpy as np
-from ...units import UnitValue, mean
+from ...units import UnitValue
 
 class emittance():
 
@@ -40,8 +40,8 @@ class emittance():
         cov_x_xp = self.beam.covariance(x, xp)
         emittance = np.sqrt(cov_x * cov_xp - cov_x_xp**2) if (cov_x * cov_xp - cov_x_xp**2) > 0 else 0
         if p is not None:
-            beta = mean(self.beam.Bz)
-            gamma = mean(p)/(self.beam.E0_eV * beta)
+            beta = np.mean(self.beam.Bz)
+            gamma = np.mean(p)/(self.beam.E0_eV * beta)
             emittance = gamma*emittance
         return emittance
 
@@ -81,7 +81,7 @@ class emittance():
     @property
     def normalized_horizontal_emittance_90(self):
         emit = self.beam.horizontal_emittance_90
-        return mean(self.beam.cp)/self.beam.E0_eV * emit
+        return np.mean(self.beam.cp)/self.beam.E0_eV * emit
     @property
     def vertical_emittance_90(self):
         emit = self.beam.vertical_emittance
@@ -93,7 +93,7 @@ class emittance():
     @property
     def normalized_vertical_emittance_90(self):
         emit = self.beam.vertical_emittance_90
-        return mean(self.beam.cp)/self.beam.E0_eV * emit
+        return np.mean(self.beam.cp)/self.beam.E0_eV * emit
 
     @property
     def horizontal_emittance_corrected(self):

SimulationFramework/Modules/Beams/Particles/sigmas.py
@@ -1,7 +1,7 @@
 import munch
 import numpy as np
 from ... import constants
-from ...units import UnitValue, mean, std
+from ...units import UnitValue
 
 class sigmas(munch.Munch):
 
@@ -38,8 +38,8 @@ class sigmas(munch.Munch):
         return np.sqrt(self.beam.covariance(self.beam.t, self.beam.t))
     @property
     def momentum_spread(self):
-        return std(self.beam.cp)
+        return np.std(self.beam.cp)
         # return self.beam.cp.std()/np.mean(self.beam.cp)
     @property
     def linear_chirp_z(self):
-        return -1*std(self.beam.Bz*self.beam.speed_of_light*self.beam.t)/self.momentum_spread/100
+        return -1*np.std(self.beam.Bz*self.beam.speed_of_light*self.beam.t)/self.momentum_spread/100