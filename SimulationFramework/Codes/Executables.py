56e4db288bd2329912060f99d8eddd891e5e5d60
jkj62 <james.jones@stfc.ac.uk>
2021-07-15 16:44:36 +0100

    Changes to Executables.py and new YAML file

--- SimulationFramework/Codes/Executables.py
+++ SimulationFramework/Codes/Executables.py
@@ -1,5 +1,6 @@
 import socket
 import os
+import yaml
 
 def which(program):
     def is_exe(fpath):
@@ -17,12 +18,45 @@ def which(program):
 
     return None
 
+class executable:
+
+    def __init__(self, name, settings={}, location=None, ncpu=1, default=''):
+        self.name = name
+        self.settings = settings
+        self.location = location
+        self.ncpu = ncpu
+        if location is not None:
+            if isinstance(location,str):
+                self.executable = [location]
+            elif isinstance(location,list):
+                self.executable = location
+        elif os.name in self.settings:
+            self.executable = self._subsitute_variables(self.settings[os.name][name])
+        else:
+            self.executable =  self._subsitute_variables(default)
+
+    def _subsitute_variables(self, param):
+        if isinstance(param, list):
+            return [self._subsitute_variables(s) for s in param]
+        else:
+            return self._subsitute_ncpu(self._subsitute_simcodes(param))
+
+    def _subsitute_simcodes(self, param):
+        if isinstance(param, list):
+            return [self._subsitute_simcodes(s) for s in param]
+        else:
+            return param.replace('$simcodes$', self.settings['sim_codes_location'])
+
+    def _subsitute_ncpu(self, param):
+        if isinstance(param, list):
+            return [self._subsitute_ncpu(s) for s in param]
+        else:
+            return param.replace('$ncpu$', str(self.ncpu))
+
 class Executables(object):
 
     def __init__(self, global_parameters):
         super(Executables, self).__init__()
-        self.osname = os.name
-        self.hostname = socket.gethostname()
         self.global_parameters = global_parameters
         sim_codes = self.global_parameters['simcodes_location'] if 'simcodes_location' in self.global_parameters else None
         if sim_codes is None:
@@ -30,6 +64,12 @@ class Executables(object):
             # print('Using SimCodes at ', os.path.abspath(self.sim_codes_location))
         else:
             self.sim_codes_location = sim_codes
+        try:
+            with open(os.path.join(os.path.dirname(__file__), '../Executables.yaml'), 'r') as file:
+                self.settings = yaml.load(file, Loader=yaml.Loader)
+        except:
+            self.settings = {}
+        self.settings['sim_codes_location'] = self.sim_codes_location
         self.define_ASTRAgenerator_command()
         self.define_astra_command()
         self.define_elegant_command()
@@ -46,102 +86,28 @@ class Executables(object):
             return ncpu
 
     def define_ASTRAgenerator_command(self, location=None):
-        if location is not None:
-            if isinstance(location,str):
-                self.ASTRAgenerator = [location]
-            elif isinstance(location,list):
-                self.ASTRAgenerator = location
-        elif not self.osname == 'nt':
-            if 'apclara1' in self.hostname:
-                self.ASTRAgenerator =  ['/opt/ASTRA/generator.sh']
-            elif 'apclara2' in self.hostname:
-                self.ASTRAgenerator =  ['/opt/ASTRA/generator.sh']
-            elif 'apclara3' in self.hostname:
-                self.ASTRAgenerator =  ['/opt/ASTRA/generator.sh']
-        else:
-            self.ASTRAgenerator =  [self.sim_codes_location+'ASTRA/generator']
+        self.ASTRAgeneratorExecutable = executable('astragenerator', settings=self.settings, location=location, default=[self.sim_codes_location+'ASTRA/generator'])
+        self.ASTRAgenerator =  self.ASTRAgeneratorExecutable.executable
 
     def define_astra_command(self, location=None, ncpu=1, scaling=None):
         ncpu = self.getNCPU(ncpu, scaling)
-        if location is not None:
-            if isinstance(location,str):
-                self.astra = [location]
-            elif isinstance(location,list):
-                self.astra = location
-        elif not self.osname == 'nt':
-            if 'apclara1' in self.hostname:
-                self.astra = ['mpiexec','-np',str(ncpu),'/opt/ASTRA/astra_MPICH2.sh']
-            elif 'apclara2' in self.hostname:
-                self.astra =  ['salloc','-n',str(ncpu),'/usr/lib64/compat-openmpi16/bin/mpirun','/opt/ASTRA/astra_r62_Linux_x86_64_OpenMPI_sld6']
-            elif 'apclara3' in self.hostname:
-                self.astra =  ['salloc','-n',str(ncpu),'/usr/lib64/compat-openmpi16/bin/mpirun','/opt/ASTRA/astra_r62_Linux_x86_64_OpenMPI_sld6']
-        else:
-            if int(self.global_parameters['astra_use_wsl']) > 1:
-                # print('WSL ASTRA in use! ncpu = ', self.global_parameters['astra_use_wsl'])
-                wsl_cpu = int(self.global_parameters['astra_use_wsl'])
-                self.astra =  ['wsl','mpiexec','-np',str(wsl_cpu),'/opt/ASTRA/astra_MPICH2.sh']
-            else:
-                # print('Serial ASTRA in use!')
-                self.astra =  [self.sim_codes_location+'ASTRA/astra']
+        self.astraExecutable = executable('astra', settings=self.settings, location=location, ncpu=ncpu, default=[self.sim_codes_location+'ASTRA/astra'])
+        self.astra = self.astraExecutable.executable
 
     def define_elegant_command(self, location=None, ncpu=1, scaling=None):
         ncpu = self.getNCPU(ncpu, scaling)
-        if location is not None:
-            if isinstance(location,str):
-                self.elegant = [location]
-            elif isinstance(location,list):
-                self.elegant = location
-        elif ncpu > 1:
-            if not self.osname == 'nt':
-                if 'apclara1' in self.hostname:
-                    self.elegant = ['/opt/MPICH2-3.2/bin/mpiexec','-np',str(ncpu),'Pelegant']
-                elif 'apclara2' in self.hostname:
-                    self.elegant = ['salloc','-n',str(ncpu),'/usr/lib64/openmpi3/bin/mpiexec','/usr/bin/Pelegant']
-                elif 'apclara3' in self.hostname:
-                    self.elegant = ['salloc','-w','apclara3','-n',str(ncpu),'/usr/lib64/openmpi3/bin/mpiexec','Pelegant']
-            else:
-                if which('mpiexec.exe') is not None and which('Pelegant.exe') is not None:
-                    self.elegant = [which('mpiexec.exe'),'-np',str(min([2,int(ncpu/3)])), which('Pelegant.exe')]
-                else:
-                    self.elegant = [self.sim_codes_location+'Elegant/elegant']
+        if ncpu > 1:
+            self.elegantExecutable = self.executable('Pelegant', settings=self.settings, location=location, ncpu=ncpu, default=[which('mpiexec.exe'),'-np',str(min([2,int(ncpu/3)])), which('Pelegant.exe')])
         else:
-            if not self.osname == 'nt':
-                if 'apclara1' in self.hostname:
-                    self.elegant = ['elegant']
-                elif 'apclara2' in self.hostname:
-                    self.elegant = ['srun','elegant']
-                elif 'apclara3' in self.hostname:
-                    self.elegant = ['srun','elegant']
-            else:
-                self.elegant = [self.sim_codes_location+'Elegant/elegant']
+            self.elegantExecutable = executable('elegant', settings=self.settings, location=location, ncpu=ncpu, default=[self.sim_codes_location+'Elegant/elegant'])
+        self.elegant = self.elegantExecutable.executable
 
     def define_csrtrack_command(self, location=None, ncpu=1, scaling=None):
         ncpu = self.getNCPU(ncpu, scaling)
-        if location is not None:
-            if isinstance(location,str):
-                self.csrtrack = [location]
-            elif isinstance(location,list):
-                self.csrtrack = location
-        elif not self.osname == 'nt':
-            if 'apclara1' in self.hostname:
-                self.csrtrack = ['/opt/OpenMPI-1.4.3/bin/mpiexec','-n',str(ncpu),'/opt/CSRTrack/csrtrack_openmpi.sh']
-            elif 'apclara2' in self.hostname:
-                self.csrtrack = ['/opt/OpenMPI-1.4.5/bin/mpiexec','-n',str(ncpu),'/opt/CSRTrack/csrtrack_1.204_Linux_x86_64_OpenMPI_1.4.3']
-            elif 'apclara3' in self.hostname:
-                self.csrtrack = ['/opt/OpenMPI-1.4.5/bin/mpiexec','-n',str(ncpu),'/opt/CSRTrack/csrtrack_1.204_Linux_x86_64_OpenMPI_1.4.3']
-        else:
-            self.csrtrack = [self.sim_codes_location+'CSRTrack/csrtrack']
+        self.csrtrackExecutable = executable('csrtrack', settings=self.settings, location=location, ncpu=ncpu, default=[self.sim_codes_location+'CSRTrack/csrtrack'])
+        self.csrtrack = self.csrtrackExecutable.executable
 
     def define_gpt_command(self, location=None, ncpu=1, scaling=None):
         ncpu = self.getNCPU(ncpu, scaling)
-        if location is not None:
-            if isinstance(location,str):
-                self.gpt = [location]
-            elif isinstance(location,list):
-                self.gpt = location
-        elif not self.osname == 'nt':
-            # print('gpt on apclara3')
-            self.gpt = ['/opt/GPT3.3.6/bin/gpt', '-j',str(ncpu)]
-            # print('gpt on apclara3', self.gpt)
-        else:
-            self.gpt = [self.sim_codes_location+'GPT/gpt.exe','-j',str(ncpu)]
+        self.gptExecutable = executable('gpt', settings=self.settings, location=location, ncpu=ncpu, default=[self.sim_codes_location+'GPT/gpt.exe','-j',str(ncpu)])
+        self.gpt = self.gptExecutable.executable
