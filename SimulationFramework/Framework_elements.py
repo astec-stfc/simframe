10afd701e18c9331d71de10f7da0ab7196389d3d
James Jones <james.jones@stfc.ac.uk>
2024-08-06 16:28:24 +0100

    Update Framework_elements.py

--- SimulationFramework/Framework_elements.py
+++ SimulationFramework/Framework_elements.py
@@ -732,7 +732,7 @@ class rf_deflecting_cavity(cavity):
         etype = self._convertType_Elegant(self.objecttype)
         string = self.objectname+': '+ etype
         for key, value in list(merge_two_dicts(self.objectproperties, self.objectdefaults).items()):
-            print('RFDF before', key, value)
+            # print('RFDF before', key, value)
             if not key == 'name' and not key == 'type' and not key == 'commandtype' and self._convertKeword_Elegant(key) in elements_Elegant[etype]:
                 value = getattr(self, key) if hasattr(self, key) and getattr(self, key) is not None else value
                 key = self._convertKeword_Elegant(key).lower()
@@ -744,7 +744,7 @@ class rf_deflecting_cavity(cavity):
                     print('WARNING: Cavity n_bins is not zero - check log file to ensure correct behaviour!')
                 value = 1 if value is True else value
                 value = 0 if value is False else value
-                print('RFDF after', key, value)
+                # print('RFDF after', key, value)
                 tmpstring = ', '+key+' = '+str(value)
                 if len(string+tmpstring) > 76:
                     wholestring+=string+',&\n'
