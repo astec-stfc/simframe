e028486772ce1b36542fbbe87b98cd18a464658b
James Jones <james.jones@stfc.ac.uk>
2025-02-04 15:21:27 +0000

    Update GPT.py

--- SimulationFramework/Codes/GPT/GPT.py
+++ SimulationFramework/Codes/GPT/GPT.py
@@ -157,12 +157,12 @@ class gptLattice(frameworkLattice):
                         )
                     screen0pos = 0
                     ccs = new_ccs
-        # if not isinstance(element, screen):
-        element = self.endScreenObject = self.endScreen()
-        fulltext += self.endScreenObject.write_GPT(self.Brho, ccs=ccs, output_ccs="wcs")
-        # else:
-        #     # print('End screen', element.objectname)
-        #     self.endScreenObject = None
+        if not isinstance(element, screen):
+            element = self.endScreenObject = self.endScreen()
+            fulltext += self.endScreenObject.write_GPT(self.Brho, ccs=ccs, output_ccs="wcs")
+        else:
+            # print('End screen', element.objectname)
+            self.endScreenObject = None
         relpos, relrot = ccs.relative_position(
             element.position_end, element.global_rotation
         )
