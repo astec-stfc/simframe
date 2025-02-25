fd87aa0bc089c0df0a5dd70337836afa8af62373
James Jones <james.jones@stfc.ac.uk>
2025-02-25 11:36:09 +0000

    Update merge_two_dicts.py

--- SimulationFramework/Modules/merge_two_dicts.py
+++ SimulationFramework/Modules/merge_two_dicts.py
@@ -13,3 +13,11 @@ def merge_two_dicts(y, x):
         z = x.copy()  # start with x's keys and values
         z.update(y)  # modifies z with y's keys and values & returns None
         return z
+
+
+def merge_dicts(*dicts: dict) -> dict:
+    """Combine multiple dictionaries: first dictionary overwrites keys in the second dictionary and so on"""
+    final_dict = dicts[-1]
+    for dic in list(reversed(dicts))[1:]:
+        final_dict = merge_two_dicts(dic, final_dict)
+    return final_dict
